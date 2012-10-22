448c70449cc7617a6f7752b8cfd4ca0113eb0137
Brian Jacobs <briantjacobs@gmail.com>
2012-10-22 18:21:52 -0400

    successful non-determinisitc accessor reference using .out of stack

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,27 +9,29 @@ bus = {
 		var margin = {top: 20, right: 20, bottom: 20, left: 20},
 			width = 760,
 			height = 500,
-			xValue = function(d) { return d[0]; },
-			yValue = function(d) { return d[1]; },
+			getX = function(d) { return d._x; },
+			getY = function(d) { return d._y; },
 			xScale = d3.time.scale(),
 			yScale = d3.scale.linear();
 
 		function chart(selection) {
 			selection.each(function(data) {
-				// Convert data to standard representation greedily;
-				// this is needed for nondeterministic accessors.
 				data = data.map(function(d, i) {
-					return [xValue.call(data, d, i), yValue.call(data, d, i)];
+					// Convert data to standard representation greedily;
+					// this is needed for nondeterministic accessors.
+					d._x = getX.call(data.values, d, i);
+					d._y = getY.call(data.values, d, i);
+					return d;
 				});
 
 				// Update the x-scale.
 				xScale
-					.domain(d3.extent(data, function(d) { return d[0]; }))
+					.domain(d3.extent(data, getX))
 					.range([0, width - margin.left - margin.right]);
 
 				// Update the y-scale.
 				yScale
-					.domain([0, d3.max(data, function(d) { return d[1]; })])
+					.domain([0, d3.max(data, getY)])
 					.range([height - margin.top - margin.bottom, 0]);
 
 				// Select the svg element, if it exists.
@@ -79,14 +81,14 @@ bus = {
 			});
 		}
 
-		// The x-accessor for the path generator; xScale âˆ˜ xValue.
+		// The x-accessor for the path generator; xScale âˆ˜ getX.
 		function X(d) {
-			return xScale(d[0]);
+			return xScale(d._x);
 		}
 
-		// The x-accessor for the path generator; yScale âˆ˜ yValue.
+		// The x-accessor for the path generator; yScale âˆ˜ getY.
 		function Y(d) {
-			return yScale(d[1]);
+			return yScale(d._y);
 		}
 
 		chart.margin = function(_) {
@@ -108,14 +110,14 @@ bus = {
 		};
 
 		chart.x = function(_) {
-			if (!arguments.length) return xValue;
-			xValue = _;
+			if (!arguments.length) return getX;
+			getX = _;
 			return chart;
 		};
 
 		chart.y = function(_) {
-			if (!arguments.length) return yValue;
-			yValue = _;
+			if (!arguments.length) return getY;
+			getY = _;
 			return chart;
 		};
 
@@ -127,33 +129,41 @@ barChart : function() {
 			width = 420,
 			height = 420,
 			yRoundBands = 0.2,
-			yValue = function(d) { return d[0]; },
-			xValue = function(d) { return d[1]; },
+			getY = function(d) { return d._y; },
+			getX = function(d) { return d._x; },
+			getC = function(d) { return d.color; },
 			yScale = d3.scale.ordinal(),
 			xScale = d3.scale.linear(),
+			forceArr = [];
 			yAxis = d3.svg.axis().scale(yScale).orient("left"),
 			xAxis = d3.svg.axis().scale(xScale),
-			color = bus.genColor();
-			
+			color = bus.genColor(),
+			dispatch =  d3.dispatch('barMouseOver');
 
 	function chart(selection) {
 		selection.each(function(data) {
-
-			// Convert data to standard representation greedily;
-			// this is needed for nondeterministic accessors.
-			data = data.map(function(d, i) {
-				return [yValue.call(data, d, i), xValue.call(data, d, i)];
+			data = data.values.map(function(d, i) {
+				// Convert data to standard representation greedily;
+				// this is needed for nondeterministic accessors.
+				// nondeterminisitic = accessor input functions that dont infuse their 
+				// variable names when executed within this scope
+				d._x = getX.call(data.values, d, i);
+				d._y = getY.call(data.values, d, i);
+				d.color = getC.call(data.values, d, i);
+				return d;
 			});
-		
+			data.sort(function(a, b) {  return b._x - a._x; });
+
+
 			// Update the y-scale.
 			yScale
-					.domain(data.map(function(d) { return d[0];} ))
+					.domain(data.map(getY))
 					.rangeRoundBands([0, width - margin.left - margin.right], yRoundBands);
 				 
 
 			// Update the x-scale.
 			xScale
-					.domain([0,d3.max(data.map(function(d) { return d[1];} ))])
+					.domain([0,d3.max(data.map(getX ))])
 					.range([0,height - margin.top - margin.bottom])
 					.nice();
 					
@@ -185,7 +195,7 @@ barChart : function() {
 					.attr("y", function(d) { return Y(d); })
 					.attr("x", function(d, i) { return 0; })
 					.attr("height", yScale.rangeBand())
-					.style("fill", function(d,i) { return color(d,i); })
+					.style("fill", getC)
 					.attr("width", function(d, i) { return Math.abs( X(d) - X0() ); });
 
 			// y axis at the bottom of the chart
@@ -195,23 +205,33 @@ barChart : function() {
 			// Update the x-axis.
 			g.select(".x.axis").transition().duration(500)
 				.call(xAxis.orient("top"));
-					
+				
+			//events
+			bar.on("mouseover", function(d,i) {
+				d3.select("#area-"+i).classed("active", true)
+			});
+
+			bar.on("mouseout", function(d,i) {
+				d3.select("#area-"+i).classed("active", false)
+			});
+
+
 		});
 	}
 
 
-// The y-accessor for the path generator; xScale ∘ xValue.
+	// The y-accessor for the path generator; xScale ∘ getX.
 	function Y(d) {
-		return yScale(d[0]);
+		return yScale(d._y);
 	}
 
 	function X0() {
 		return xScale(0);
 	}
 
-	// The x-accessor for the path generator; yScale ∘ yValue.
+	// The x-accessor for the path generator; yScale ∘ getY.
 	function X(d) {
-		return xScale(d[1]);
+		return xScale(d._x);
 	}
 
 	chart.margin = function(_) {
@@ -233,14 +253,14 @@ barChart : function() {
 	};
 
 	chart.x = function(_) {
-		if (!arguments.length) return xValue;
-		xValue = _;
+		if (!arguments.length) return getX;
+		getX = _;
 		return chart;
 	};
 
 	chart.y = function(_) {
-		if (!arguments.length) return yValue;
-		yValue = _;
+		if (!arguments.length) return getY;
+		getY = _;
 		return chart;
 	};
 
@@ -251,51 +271,71 @@ barChart : function() {
 		var margin = {top: 20, right: 20, bottom: 20, left: 20},
 			width = 760,
 			height = 500,
-			xValue = function(d) { return d.x; },
-			yValue = function(d) { return d.y; },
+			getX = function(d) { return d._x; },
+			getY = function(d) { return d._y; },
 			getValues = function(d) { return d.values; },
 			xScale = d3.time.scale(),
 			yScale = d3.scale.linear(),
 			area = d3.svg.area(),
 			stack =  d3.layout.stack().offset("wiggle").order('inside-out'),
-			color = bus.genColor();
+			color = bus.genColor(),
+			dispatch =  d3.dispatch('sliceMouseOver');
+
+
 		function chart(selection) {
 			selection.each(function(data,i) {
-
-				data = data.map(function(d) {
-							d.values = d.values.map(function(j,i) {
-								return { type:d.type, x: xValue.call(data, j, i), y: yValue.call(data, j, i)};
+				data = data.map(function(d,index) {
+							d.values = d.values.map(function(dd,i) {
+								//this ensuring that the updated accessor values point to the right names
+								dd._x = getX.call(data, dd, i);
+								dd._y = getY.call(data, dd, i);
+								//store index for proper highlighting
+								dd.index = i;
+								dd.color = color(d,index);
+								return dd;
 							});
+							//store color to pass to bars
+							d.color = color(d,index);
 							return d;
 						});
+
+
 				var mergedValues = d3.merge(data.map(function(d) {return d.values;}));
 
+
+				stack
+					.x(getX)
+					.y(getY)
+					.values(getValues)
+					.out(function(d, y0, y) {
+						d.render = {
+							y: y,
+							y0: y0
+						};
+					});
 
 				area
 					.interpolate("cardinal")
 					.x(function(d){return X(d);})
-					.y0(function(d){return Y0(d);})
-					.y1(function(d) { return Y1(d); });
+					.y0(function(d) { return yScale(d.render.y0); })
+					.y1(function(d) { return yScale(d.render.y + d.render.y0); });
 
-				stack
-					.x(xValue)
-					.y(yValue)
-					.values(getValues);
-
-
-				xScale
-					.domain(d3.extent(mergedValues, function(d) { return d.x; }))
-					.range([0, width - margin.left - margin.right]);
-
-				// Update the y-scale.
-				yScale
-					.domain([0, d3.max(mergedValues, function(d) {return d.y+d.y0;})])
-					.range([height - margin.top - margin.bottom, 0]);
 
 
 
 				// Select the svg element, if it exists.
 				var svg = d3.select(this).selectAll("svg").data([stack(data)]);
+
+				xScale
+					.domain(d3.extent(mergedValues, function(d) { return d._x; }))
+					.range([0, width - margin.left - margin.right]);
+
+				// Update the y-scale.
+				yScale
+					.domain([0, d3.max(mergedValues, function(d) {return d._y+d.render.y0;})])
+					.range([height - margin.top - margin.bottom, 0]);
+
+
 
 				// Otherwise, create the skeletal chart.
 				var gEnter = svg.enter().append("svg").append("g");
@@ -314,11 +354,12 @@ barChart : function() {
 								areaPaths.exit().remove();
 								areaPaths.transition().duration(1000)
 									.attr("class", "area")
-									.style("fill", function(d,i) { return color(d,i); })
+									.attr("id", function(d,i) { return "area-"+i; })
+									.style("fill", function(d,i) { return d.color; })
 									.attr("d", function(d,i) {return area(d.values); });
 											
 				var timeLookup = d3.nest()
-									.key(function(d) { return d.x; })
+									.key(function(d) { return d._x; })
 									.entries(mergedValues);
 
 				svg.on("mousemove", function(d,i) {
@@ -330,10 +371,10 @@ barChart : function() {
 						var stopTime = new Date(h.key);
 						if (stopTime.getTime() > timePosLeft.getTime() && stopTime.getTime() < timePosRight.getTime()) {
 							var barChart = bus.barChart()
-								.x(function(d) { return d.y;  })
+								.x(function(d) { return d.render.y;  })
 								.y(function(d) { return d.type; });
 							d3.select("#bars")
-								.datum(timeLookup[i].values)
+								.datum(timeLookup[i])
 								.call(barChart);
 						}
 					});
@@ -342,14 +383,14 @@ barChart : function() {
 			});
 		}
 
-		// The x-accessor for the path generator; xScale âˆ˜ xValue.
+		// The x-accessor for the path generator; xScale âˆ˜ getX.
 		function X(d) {
-			return xScale(d.x);
+			return xScale(d._x);
 		}
 
-		// The x-accessor for the path generator; yScale âˆ˜ yValue.
+		// The x-accessor for the path generator; yScale âˆ˜ getY.
 		function Y1(d) {
-			return yScale(d.y + d.y0);
+			return yScale(d._y + d.y0);
 		}
 
 		function Y0(d) {
@@ -375,14 +416,14 @@ barChart : function() {
 		};
 
 		chart.x = function(_) {
-			if (!arguments.length) return xValue;
-			xValue = _;
+			if (!arguments.length) return getX;
+			getX = _;
 			return chart;
 		};
 
 		chart.y = function(_) {
-			if (!arguments.length) return yValue;
-			yValue = _;
+			if (!arguments.length) return getY;
+			getY = _;
 			return chart;
 		};
 
@@ -398,7 +439,7 @@ barChart : function() {
 $(function(){
 
 
-	d3.json("scripts/test_stream.json", function(data) {
+	d3.json("scripts/test_stream.json", function(source) {
 		var formatDate = d3.time.format("%H:%M%p");
 
 		var streamPlot = bus.streamPlot()
@@ -412,7 +453,7 @@ $(function(){
 							.y(function(d) { return d.y; });
 
 		d3.select("#stream")
-			.datum(data)
+			.datum(source)
 			.call(streamPlot);
 
 		d3.json("scripts/test_stream2b.json", function(data2) {
@@ -423,7 +464,7 @@ $(function(){
 			});
 		});
 
-		d3.json("scripts/23_stops.json", function(data) {
+		d3.json("scripts/23_stops.json", function(source) {
 		//bus.data = data;
 		var formatDate = d3.time.format("%H:%M%p");
 		var stopPlot = bus.stopPlot()
@@ -431,7 +472,7 @@ $(function(){
 				.y(function(d) { return +Math.random(10); });
 
 			d3.select("#stream")
-				.datum(data)
+				.datum(source)
 				.call(stopPlot);
 		});
 
